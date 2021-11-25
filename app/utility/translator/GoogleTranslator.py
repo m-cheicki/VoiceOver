2224c5213a0bff456e439c5c0e50c7826a6f7729
Cédric Riana Rakotoarisoa <cedricriana@gmail.com>
2021-11-25 16:43:05 +0100

    Resolve translation bug with google translator

--- app/utility/translator/GoogleTranslator.py
+++ app/utility/translator/GoogleTranslator.py
@@ -1,12 +1,11 @@
 from googletrans import Translator
 
-translator = Translator()
-
 def translate(text, target_language):
     """
     Translate text to target language using Google Translate API
     :param text: text to translate
     :param target_language: target language
     """
+    translator = Translator()
     translation = translator.translate(text, dest=target_language)
     return translation.text
